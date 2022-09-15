e86ea40a5b96350e1899908a0002a4ebc13b8d6a
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-09-16 04:06:47 +0900

    cors直った

--- go.mod
+++ go.mod
@@ -6,16 +6,19 @@ require (
 	github.com/99designs/gqlgen v0.17.16
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/joho/godotenv v1.4.0
-	github.com/vektah/gqlparser/v2 v2.5.0
+	github.com/vektah/gqlparser/v2 v2.5.1
 	gorm.io/driver/mysql v1.3.6
 	gorm.io/gorm v1.23.8
 )
 
 require (
+	github.com/99designs/gqlgen/_examples v0.0.0-20220915150518-b7cc094a49e3 // indirect
 	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/go-chi/chi v1.5.4 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mitchellh/mapstructure v1.3.1 // indirect
+	github.com/rs/cors v1.8.2 // indirect
 )
--- server.go
+++ server.go
@@ -1,36 +1,47 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
-	"github.com/go-chi/chi"
-	"github.com/rs/cors"
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
+
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"hackz.com/m/v2/graph"
 	"hackz.com/m/v2/graph/generated"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
+	"github.com/gorilla/websocket"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
-const defaultPort = "8080"
-
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
+	router := chi.NewRouter()
 
+	// Add CORS middleware around every request
+	// See https://github.com/rs/cors for full option listing
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+	
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-
-	c := cors.New(cors.Options{
-        AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:8080"},
-        AllowCredentials: true,
+    srv.AddTransport(&transport.Websocket{
+        Upgrader: websocket.Upgrader{
+            CheckOrigin: func(r *http.Request) bool {
+                // Check against your desired domains here
+                 return r.Host == "example.org"
+            },
+            ReadBufferSize:  1024,
+            WriteBufferSize: 1024,
+        },
     })
 
-	http.Handle("/query", c.Handler(srv))
+	router.Handle("/", playground.Handler("Starwars", "/query"))
+	router.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		panic(err)
+	}
 }
+
