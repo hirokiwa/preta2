d94d34cf8956ad592f9815147cda0e0624866b16
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-09-16 01:15:40 +0900

    cors対応

--- server.go
+++ server.go
@@ -4,7 +4,8 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"hackz.com/m/v2/graph"
@@ -22,7 +23,13 @@ func main() {
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+
+	c := cors.New(cors.Options{
+        AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:8080"},
+        AllowCredentials: true,
+    })
+
+	http.Handle("/query", c.Handler(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
